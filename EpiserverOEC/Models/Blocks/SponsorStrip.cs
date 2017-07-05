7a5126b42e05df8b622c3ea41728edd865083cb1
Per <per.jenelius@gmail.com>
2017-07-05 16:51:56 +0200

    Lite jobb på sponsorremsan

---
+++ EpiserverOEC/Models/Blocks/SponsorStrip.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EpiserverOEC.Models.Blocks
+{
+    [ContentType(DisplayName = "SponsorStrip", GUID = "215a3000-f8de-4d7e-9d3c-951d539c1972", Description = "")]
+    public class SponsorStrip : BlockData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Heading",
+            Description = "Add a heading.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual String Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Body Text",
+            Description = "Add some text to describe the page.",
+            GroupName = SystemTabNames.Content,
+            Order = 2)]
+        public virtual XhtmlString BodyText { get; set; }
+    }
+}
