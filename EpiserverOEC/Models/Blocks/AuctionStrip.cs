95c1203a8a7d3411339d5b0b9903c7cb63d5d52e
Per <per.jenelius@gmail.com>
2017-07-06 09:22:16 +0200

    SponsorGrejan fixad. Info fixad. Auktionsgrejan påbörjad.

---
+++ EpiserverOEC/Models/Blocks/AuctionStrip.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace EpiserverOEC.Models.Blocks
+{
+    [ContentType(DisplayName = "AuctionStrip", GUID = "38123cfc-0bbe-45f0-9929-505a9515f218", Description = "")]
+    public class AuctionStrip : BlockData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Heading",
+            Description = "Add a heading.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual String Heading { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Body Text",
+            Description = "Add some text to describe the page.",
+            GroupName = SystemTabNames.Content,
+            Order = 2)]
+        public virtual XhtmlString BodyText { get; set; }
+    }
+}
