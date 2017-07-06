44a18374a3dbc0885a5678a55e4bc81b459fac16
Per <per.jenelius@gmail.com>
2017-07-06 16:25:11 +0200

    Blogsidan layoutad men inte funktionell

---
+++ EpiserverOEC/Models/YearMonth.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpiserverOEC.Models
+{
+    public class YearMonth
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+
+        public YearMonth(int year, int month)
+        {
+
+        }
+    }
+}
