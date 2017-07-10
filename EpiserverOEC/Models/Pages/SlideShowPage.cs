ab67984385164ef2ecb0baae2f307bec12790c8b
Per <per.jenelius@gmail.com>
2017-07-10 13:37:26 +0200

    Fixat till js på bloggsidan. Påbörjat bildspelet.

---
+++ EpiserverOEC/Models/Pages/SlideShowPage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EpiserverOEC.Models.Pages
+{
+    [ContentType(DisplayName = "SlideShowPage", GUID = "cd0059b0-f2ed-4d23-a1ba-95c0a8ffea7f", Description = "")]
+    public class SlideShowPage : SitePageData
+    {
+    }
+}
